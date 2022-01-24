f8efb3f71566be5924956f030fe0c375e7822f97
bala.vepuri <balakrishnavepuri@gmail.com>
2022-01-24 07:52:38 +0000

    terraform-eks

---
+++ cluster/variable.tf
@@ -0,0 +1,30 @@
+#
+# Variables Configuration
+#
+variable "cluster-name" {}
+
+variable "vpc_id" {
+  description = "VPC ID "
+}
+
+variable "eks_subnets" {
+  description = "Master subnet ids"
+  type        = "list"
+}
+
+variable "worker_subnet" {
+  type = list(string)
+}
+
+variable "subnet_ids" {
+type        = list(string)
+  
+     
+    
+  
+  description = "List of all subnet in cluster"
+}
+
+variable "kubernetes-server-instance-sg" {
+  description = "Kubenetes control server security group"
+}
